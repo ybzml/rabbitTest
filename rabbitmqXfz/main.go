433c8486f78bd8db6ebaaf27f36aa6aed21a7c40
ybzml <1619877554@qq.com>
2023-04-30 15:02:40 +0800

    ma1 loacom1

--- rabbitmqXfz/main.go
+++ rabbitmqXfz/main.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"github.com/streadway/amqp"
 	"log"
+	"sync"
 )
+
+var wg sync.WaitGroup
 
 func main() {
 	connction, err1 := amqp.Dial("amqp://admin:123@39.107.239.138:5672/")
@@ -22,15 +25,25 @@ func main() {
 		false,
 		false,
 		nil)
-
+	wg.Add(2)
+	go func() {
+		for msg1 := range createAndCusQue(channel, "topicwwww.#") {
+			fmt.Printf("ms1:%s\n", msg1.Body)
+		}
+	}()
+	go func() {
+		for msg2 := range createAndCusQue(channel, "topickkkk.#") {
+			fmt.Printf("ms2:%s\n", msg2.Body)
+		}
+	}()
+	wg.Wait()
+}
+func createAndCusQue(channel *amqp.Channel, routing_key string) <-chan amqp.Delivery {
 	queue, err := channel.QueueDeclare("", false, false, false, false, nil)
-
-	channel.QueueBind(queue.Name, "direct_key", "derectEx", false, nil)
-
+	channel.QueueBind(queue.Name, routing_key, "topicEx", false, nil)
 	if err != nil {
 		log.Fatal("queue  failed! %v\n", err)
 	}
 	consume, _ := channel.Consume(queue.Name, "", false, false, false, false, nil)
-	msg := <-consume
-	fmt.Printf("received msg:%s\n", msg.Body)
+	return consume
 }
